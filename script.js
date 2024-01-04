bd5c6016c6f5952952c54174268bfbb47dc7e0b7
Sakshi <144778485+thesakshijaiswal@users.noreply.github.com>
2024-01-04 10:09:20 +0530

    Created function named playpause

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ song.onloadedmetadata = function () {
   progress.max = song.duration;
   progress.value = song.currentTime;
 };
+function playPause() {
+  if (ctrlBtn.classList.contains("ri-pause-line")) {
+    song.pause();
+    ctrlBtn.classList.add("ri-play-fill");
+    ctrlBtn.classList.remove("ri-pause-line");
+  } else {
+    song.play();
+    ctrlBtn.classList.remove("ri-play-fill");
+    ctrlBtn.classList.add("ri-pause-line");
+  }
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  ctrlBtn.classList.remove("ri-play-fill");
+  ctrlBtn.classList.add("ri-pause-line");
+};
